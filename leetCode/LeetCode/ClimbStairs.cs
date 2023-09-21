d3d670e33c2a8eb00cc3f35951e0455c56329f7d
mushir khan <mushir.khan@cimpress.com>
2023-09-21 11:47:23 +0530

    added climb stair

---
+++ leetCode/LeetCode/ClimbStairs.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    public class ClimbStairs
+    {
+        public int ClimbStair(int n)
+        {
+            int one = 1;
+            int two = 1;
+
+            for (int i = 0; i < n - 1; i++)
+            {
+                int temp = one;
+                one = one + two;
+                two = temp;
+            }
+
+            return one;
+        }
+    }
+}
